5819dcd9dbf18daf84e17cb1c1e77e774912b743
steephengeorge <steephengeorge@gmail.com>
2021-01-03 17:55:43 -0800

    register-service

---
+++ register-service/src/main/java/com/sm/registerservice/controller/AddressController.java
@@ -0,0 +1,15 @@
+package com.sm.registerservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registerSvc/api")
+public class AddressController {
+	@GetMapping("/address")
+	public String getAddress() {
+		return "Address1";
+	}
+
+}
