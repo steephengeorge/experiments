f658b654ea5c69b947385819ccf10270e01446f9
steephengeorge <steephengeorge@gmail.com>
2021-01-06 05:57:36 -0800

    inter-service communication within k8s

---
+++ search-service/src/main/java/com/sm/searchservice/controller/preferenceController.java
@@ -0,0 +1,19 @@
+package com.sm.searchservice.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/searchSvc/api")
+public class preferenceController {
+	
+	@GetMapping("/preferences")
+	public List<String> getPreferences(){
+		return List.of("reading", "music");
+	}
+
+}
+
